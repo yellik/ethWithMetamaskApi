1c981d1c442dadde1f88228d83468a2445f5524e
Yelena Kensborn <yelenakensborn@gmail.com>
2024-02-15 11:20:33 +0100

    implemented transaction logic

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const accountInput = document.querySelector('#accountNumber');
 const checkBalanceButton = document.querySelector('#checkBalance');
 const displayBalance = document.querySelector('#balance');
@@ -22,7 +23,23 @@ async function checkBalance() {
    }
     
    async function sendFunds() {
+    
      try {
+      const amount = parseFloat(valueInput.value) * Math.pow(10, 18)
+      let params = [
+        {
+        from: accountInput.value,
+        to: toAccountInput.value,
+        value: Number(amount).toString(16),
+        gas: Number(21000).toString(16),
+        gasPrice: Number(250000).toString(16),
+      },
+    ];
+        //make the transaction
+    await ethereum.request({
+      method: 'eth_sendTransaction',
+      params: params,
+    })
      } catch (error) {
        console.log(error);
      }
